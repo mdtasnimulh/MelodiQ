44214722bfc90240e92a87e1c44fcf323f952395
tasnimul <mdtasnimulh@gmail.com>
2024-11-11 14:23:06 +0600

    horizontal pager added to player screen

--- feature-player/src/main/java/com/tasnimulhasan/featureplayer/PlayerScreen.kt
+++ feature-player/src/main/java/com/tasnimulhasan/featureplayer/PlayerScreen.kt
@@ -1,16 +1,33 @@
 package com.tasnimulhasan.featureplayer
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import kotlin.math.absoluteValue
+import com.tasnimulhasan.designsystem.R as Res
 
 @Composable
 internal fun PlayerScreen(
@@ -18,17 +35,72 @@ internal fun PlayerScreen(
     modifier: Modifier = Modifier,
     viewModel: PlayerViewModel = hiltViewModel()
 ) {
-    Box (modifier = modifier.fillMaxSize()) {
-        Text(
-            text = viewModel.getSelectedMusic(musicId).songTitle,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth().padding(24.dp),
-        )
+    val pagerState = rememberPagerState { viewModel.musics.size }
+    val context = LocalContext.current
+
+    // Find the index of the selected music
+    val initialPageIndex = viewModel.musics.indexOfFirst { it.songId.toString() == musicId }
+    LaunchedEffect(initialPageIndex) {
+        pagerState.animateScrollToPage(initialPageIndex)
+    }
+
+    val currentPage = pagerState.currentPage
+    val currentMusic = viewModel.musics.getOrNull(currentPage)
+
+    Column(modifier = modifier.fillMaxSize()) {
+        currentMusic?.let {
+            Text(
+                text = it.songTitle,
+                textAlign = TextAlign.Center,
+                maxLines = 1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+            )
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            contentPadding = PaddingValues(horizontal = 40.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+        ) { page ->
+            val selectedMusic = viewModel.musics[page]
+            val pageOffset = (pagerState.currentPage - page + pagerState.currentPageOffsetFraction).coerceIn(-1f, 1f)
+
+            LaunchedEffect(page) {
+                viewModel.loadBitmapIfNeeded(context, page)
+            }
+
+            Card(
+                modifier = Modifier
+                    .graphicsLayer {
+                        val scale = lerp(start = 0.85f, stop = 1f, fraction = 1f - pageOffset.absoluteValue)
+                        scaleX = scale
+                        scaleY = scale
+                        alpha = lerp(start = 0.4f, stop = 1f, fraction = 1f - pageOffset.absoluteValue)
+                        translationX = lerp(start = 0f, stop = 0f, fraction = 1f - pageOffset.absoluteValue)
+                    }
+            ) {
+                AsyncImage(
+                    model = selectedMusic.cover,
+                    contentDescription = "Cover art",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(shape = MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.FillHeight,
+                    placeholder = painterResource(Res.drawable.ic_launcher_foreground),
+                    error = painterResource(Res.drawable.ic_launcher_foreground)
+                )
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PlayerScreenPreview() {
-    PlayerScreen("12345",modifier = Modifier)
+    PlayerScreen("12345", modifier = Modifier)
 }
--- feature-player/src/main/java/com/tasnimulhasan/featureplayer/PlayerViewModel.kt
+++ feature-player/src/main/java/com/tasnimulhasan/featureplayer/PlayerViewModel.kt
@@ -1,5 +1,10 @@
 package com.tasnimulhasan.featureplayer
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
+import android.net.Uri
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -8,6 +13,7 @@ import com.tasnimulhasan.domain.base.BaseViewModel
 import com.tasnimulhasan.domain.localusecase.music.FetchMusicUseCase
 import com.tasnimulhasan.entity.home.MusicEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -34,6 +40,26 @@ class PlayerViewModel @Inject constructor(
 
     fun getSelectedMusic(id: String) : MusicEntity {
         return musics.find { it.songId.toString() == id }!!
+    }
+
+    fun loadBitmapIfNeeded(context: Context, index: Int) {
+        if (musics[index].cover != null) return
+        viewModelScope.launch(Dispatchers.Default) {
+            val bitmap = getAlbumArt(context, musics[index].contentUri)
+            musics[index] = musics[index].copy(cover = bitmap)
+        }
+    }
+
+    private fun getAlbumArt(context: Context, uri: Uri): Bitmap? {
+        val mmr = MediaMetadataRetriever()
+        mmr.setDataSource(context, uri)
+        val data = mmr.embeddedPicture
+
+        return if (data != null) {
+            BitmapFactory.decodeByteArray(data, 0, data.size)
+        } else {
+            null
+        }
     }
 
 }
