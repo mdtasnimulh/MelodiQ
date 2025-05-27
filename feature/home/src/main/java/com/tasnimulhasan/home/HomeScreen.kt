a3163b55fc81b3eab9f175b04b7b137830bd35ec
Md. Tasnimul Hasan <mdtasnimulh@gmail.com>
2025-05-27 14:41:54 +0600

    player screen progress bar issue solved

--- feature/home/src/main/java/com/tasnimulhasan/home/HomeScreen.kt
+++ feature/home/src/main/java/com/tasnimulhasan/home/HomeScreen.kt
@@ -69,6 +69,7 @@ internal fun HomeScreen(
                             context.startService(intent)
                             isServiceRunning = true
                         }
+                        viewModel.onUiEvents(UIEvents.UpdateProgress(viewModel.progress / 100f))
                         navigateToPlayer(item.songId.toString())
                     }
                 )
